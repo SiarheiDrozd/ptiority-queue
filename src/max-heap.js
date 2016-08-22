9eb3dfec6ec9b0eb729ecc546fe5a55b4abf7a04
Siarhei Drozd <ggold.d.roger@gmail.com>
2016-08-22 17:47:21 +0300

    some code

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,15 +2,26 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		//some text
+		this.root = null; //set start
+		this.parentNodes = [];
+		this.heapSize = 0;
 	}
 
 	push(data, priority) {
-		
+		var newNode = new Node(data, priority);
+
+		if(this.root == null){
+			this.root = newNode;
+		}else{
+			insertNode(newNode);
+		}
+		this.heapSize++;
 	}
 
 	pop() {
-		
+		if(this.root != null){
+			return detachRoot();
+		}
 	}
 
 	detachRoot() {
@@ -22,15 +33,20 @@ class MaxHeap {
 	}
 
 	size() {
-		
+		return this.heapSize;
 	}
 
 	isEmpty() {
-		
+		// return this.root;
+		if(this.root == null)
+			return true
+		else
+			return false;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,52 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+        this.priority = priority;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
 	}
 
 	appendChild(node) {
-
+		if(this.left == null){
+			this.left = node;
+        }
+		else if(this.right == null){
+			this.right = node;
+		}
 	}
 
 	removeChild(node) {
-
+        if((this.left.data == node.data) && (this.left.priority == node.priority)){
+        	this.left = null;
+        }
+		else if ((this.right.data == node.data) && (this.right.priority == node.priority)) {
+			this.right = null;
+		}
+		else throw new Error("node isn't a child");
 	}
 
 	remove() {
 
+        if(this.parent != null){
+			var child = new Node(15, 42);
+        	this.parent.removeChild(child); //remove itself
+        }
 	}
 
 	swapWithParent() {
-		
+		if(this.parent != null) {
+			var grandParent = this.parent.parent;
+			var currentParent = this.parent;
+
+			this.appendChild(currentParent);
+
+			grandParent.removeChild(this.parent);
+			grandParent.appendChild(this);
+
+			this.parent = grandParent;
+
+		}
 	}
 }
 
